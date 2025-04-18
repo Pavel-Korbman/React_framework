77d6c346000fd2b438169d2c0017c3aa2ae8705f
Pavel <pavel@b2profit.ru>
2025-04-18 22:03:16 +0300

    2 ДЗ

---
+++ dz-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CommentsList from './components/TextComments';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">2 ДЗ</header>
+      <CommentsList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ dz-2/src/components/TextComments.js
@@ -0,0 +1,27 @@
+
+import { useState } from "react";
+
+function CommentsList() {
+    const [comments, setComments] = useState([
+        { id: 1, text: "Это первый комментарий" },
+        { id: 2, text: "Это второй комментарий" },
+        { id: 3, text: "Это третий комментарий" }
+    ]);    
+
+    const delComment = (id) => {       
+        setComments(array => array.filter(element => element.id !== id));        
+    }   
+
+    return comments.map((comment) =>
+        <div>
+            <div className="comment">
+                <p >
+                    <span>{comment.id} </span>
+                    {comment.text}</p>
+                <button onClick={()=>delComment(comment.id)} >Удалить</button>
+            </div >
+        </div>
+    );
+}
+
+export default CommentsList;
