93b758c8ac81a50b4281f03097694789cab11a80
Pavel <pavel@b2profit.ru>
2025-04-19 19:24:45 +0300

    2 Лекция

---
+++ lect-2/src/components/hooks.js
@@ -0,0 +1,9 @@
+import { useMemo } from "react";
+
+const changeCount = useCallback(() => {
+    setCount(1);
+}, []);
+
+const elem = useMemo(() => props.hugeArray.find(el => el.show),
+    [props.hugeArray]
+);
---
+++ lect-2/src/components/timer.js
@@ -0,0 +1,19 @@
+import { use, useEffect, useLayoutEffect, useState } from "react";
+
+function timer() {
+    const [time, setTime] = useState(new Date());
+
+useEffect(()=>{
+    first
+
+    return ()=>{
+        second
+    }
+}, [third])
+
+    return (  
+        
+    );
+}
+
+export default timer;
