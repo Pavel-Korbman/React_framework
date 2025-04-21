2d131bea62ed3e75bc80f872f37354699beb4ff9
Pavel <pavel@b2profit.ru>
2025-04-21 22:05:41 +0300

    3 ДЗ

--- dz-3/src/App.js
+++ dz-3/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './App.css';
+import TemperatureConverter from './Converter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       ДЗ 3
       </header>
+      <h3>1 задание:</h3>
+      <TemperatureConverter/>
     </div>
   );
 }
---
+++ dz-3/src/Converter.js
@@ -0,0 +1,60 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+
+function TemperatureConverter() {
+    const [celsius, setCelsius] = useState('');
+    const [fahrenheit, setFahrenheit] = useState('');
+
+    const inputFahrenheit = (e) => {
+        setCelsius('');
+        setFahrenheit(e.target.value);        
+    };
+
+    const inputCelsius = (e) => {
+        setFahrenheit('');
+        setCelsius(e.target.value);        
+    };
+
+    const convert = () => {
+        fahrenheit === '' ?
+            setFahrenheit(celsius * 9 / 5 + 32)
+            : setCelsius((fahrenheit - 32) * 5 / 9);
+    };
+
+
+    return (
+        <div className='converter'>
+            <Box
+                className=''
+                component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField type="number" onInput={inputCelsius} label="&deg;C" variant="outlined" size='small' value={celsius} />
+            </Box>
+
+            <div>
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained" onClick={() => convert()}>Перевести</Button>
+                </Stack>
+            </div>
+
+            <Box
+                component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField type="number" onInput={inputFahrenheit} label="&deg;F" variant="outlined" size='small' value={fahrenheit} />
+            </Box>
+        </div>
+
+    );
+}
+
+export default TemperatureConverter;
+
