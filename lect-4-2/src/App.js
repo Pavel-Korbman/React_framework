5b1a1a1c87d36cf11934112e987af4d0f258ab64
Pavel <pavel@b2profit.ru>
2025-04-22 19:43:02 +0300

    4 Лекция 2 часть

---
+++ lect-4-2/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+const articles = [
+  {id: 1, title: 'Статья 1', content: 'Содержание статьи 1'},
+  {id: 2, title: 'Статья 2', content: 'Содержание статьи 2'},
+  {id: 3, title: 'Статья 3', content: 'Содержание статьи 3'}
+]; // приходит с бэкенда
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        4 Лекция ReactRouter
+      </header>
+    </div>
+  );
+}
+
+export default App;
