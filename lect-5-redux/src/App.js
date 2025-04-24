e4d0845b1a0efcd4680e8e573185800c59f7119c
Pavel <pavel@b2profit.ru>
2025-04-24 18:49:39 +0300

    5 Лекция Redux

--- lect-5-redux/src/App.js
+++ lect-5-redux/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './components/store';
+import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Лекция 5 ч2 Redux
+        Лекция 5 ч2 Redux
       </header>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     </div>
   );
 }
