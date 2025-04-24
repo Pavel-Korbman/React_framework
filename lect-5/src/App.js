69205edb1c29f75e37de27c0b00f83b7c0e5dbfb
Pavel <pavel@b2profit.ru>
2025-04-24 16:36:15 +0300

    5 Лекция

--- lect-5/src/App.js
+++ lect-5/src/App.js
@@ -1,12 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { createContext, useContext } from 'react';
+
+
+// React.Context - для получения данных в компонент н алюбом уровне вложенности, без передачи их через промежуточные компоненты, а получая напрямую из контекста
+
+const messageContext = createContext();
+
+function GrandGrandChild() {  // компонент использует контекст
+  const message = useContext(messageContext);
+  return <span>{message}</span>;
+}
+
+function GrandParent() {  // компонент устанавливает контекст
+
+  return (
+    <messageContext.Provider value='Привет это содержание контекста'>
+      <Parent />
+    </messageContext.Provider>
+  );  // messageContext - не обязательное название - можно любое
+}
+
+// далее передаём контекст вниз к дочерним элементам:
+
+function Parent() {
+  return <Child />
+}
+
+function Child() {
+  return <GrandChild />
+}
+
+function GrandChild() {
+  return <GrandGrandChild />
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Лекция 5
+        Лекция 5
       </header>
+      <h1>Пример использования React.Context</h1>
+      <GrandParent/>
     </div>
   );
 }
