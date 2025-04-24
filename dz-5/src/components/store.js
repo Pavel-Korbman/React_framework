e7ca3fb5c7ec5e7a4c5fcea2cf6cadc25cea496f
Pavel <pavel@b2profit.ru>
2025-04-25 01:13:12 +0300

    5 ДЗ

--- dz-5/src/components/store.js
+++ dz-5/src/components/store.js
@@ -3,8 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 const reducer = (state = false, action) => {
     switch (action.type) {
         case 'TOGGLE_THEME':
-            return !state;       
-
+            return !state;     
         default:
             return state;
     }
