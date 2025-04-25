aa3124aeddee07d14e533a6e3e93c6c5dc6fe121
Pavel <pavel@b2profit.ru>
2025-04-25 17:38:15 +0300

    6 лекция

--- lect-6/src/App.js
+++ lect-6/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import React from 'react';
 import { Provider } from 'react-redux';
+import store from './store';
+import ButtonComponent from './components/ButtonComponent';
+import CounterComponent from './components/CounterComponent';
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       6 Лекция. Погружение в Redux.
+        6 Лекция. Погружение в Redux.
       </header>
+      <Provider store={store}>
+        <div>
+          <CounterComponent/>
+          <ButtonComponent/>
+        </div>
+      </Provider>
     </div>
   );
 }
