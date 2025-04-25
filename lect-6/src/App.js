41004c6ab619e0135328ec9f00790569a4818bb1
Pavel <pavel@b2profit.ru>
2025-04-25 16:09:02 +0300

    6 лекция

---
+++ lect-6/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       6 Лекция. Погружение в Redux.
+      </header>
+    </div>
+  );
+}
+
+export default App;
