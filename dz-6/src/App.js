a3c21ccd0965ff1c3725bc424b2d333c0b9b81b9
Pavel <pavel@b2profit.ru>
2025-04-26 22:42:51 +0300

    6 дз

---
+++ dz-6/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       6 ДЗ. Приложение для управления каталогом продуктов.
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
