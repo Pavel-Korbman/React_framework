07d0fa15eac9d54f91262071a0ede20de99d50b1
Pavel <pavel@b2profit.ru>
2025-04-29 19:46:39 +0300

    7 дз

---
+++ dz-7/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import {store} from './store';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       7 лекция. Redux middlewares. redux-saga
+      </header>
+      <Provider store={store}>
+        <TaskList />
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ dz-7/src/data/tasks.js
@@ -0,0 +1,18 @@
+const tasks = [
+    {
+        id: 'ID:01',
+        title: 'Задача-1',
+        completed: true
+    },
+    {
+        id: 'ID:02',
+        title: 'Задача-2',
+        completed: false
+    },
+    {
+        id: 'ID:03',
+        title: 'Задача-3',
+        completed: false
+    }
+];
+export default tasks;
