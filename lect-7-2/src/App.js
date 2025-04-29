1ca2dfbdd0d6e69693097e2d75de0a961c3037ec
Pavel <pavel@b2profit.ru>
2025-04-29 17:05:01 +0300

    7 лекция 2 часть

---
+++ lect-7-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       7 лекция. Redux middlewares. redux-saga
+      </header>
+    </div>
+  );
+}
+
+export default App;
