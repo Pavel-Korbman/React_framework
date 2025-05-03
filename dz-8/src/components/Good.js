5e53561e5f11f584b69cb045389551572a2fe18e
Pavel <pavel@b2profit.ru>
2025-05-03 22:47:16 +0300

    8 дз. Промежуточная аттестация

--- dz-8/src/components/Good.js
+++ dz-8/src/components/Good.js
@@ -7,7 +7,7 @@ function Good(props) {
             <img className="product__image" src={props.img} alt="product"></img>
             <img className="hoover__on" src={hover} alt="card"></img>
             <div className="product__content">
-                <a href="#" className="product__name">{props.name}</a>
+                <a href="/product" className="product__name">{props.name}</a>
                 <p className="product__text">{props.text}</p>
                 <p className="product__prise">${props.prise.toFixed(2)}</p>
             </div>
